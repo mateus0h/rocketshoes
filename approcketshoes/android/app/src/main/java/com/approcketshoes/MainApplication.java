7ce14301ba7442fa71b9f6e7d60e38fe8c9f968e
Mateus <mateus3u@gmail.com>
2020-02-10 14:40:06 -0300

    corrigindo code-push

--- approcketshoes/android/app/src/main/java/com/approcketshoes/MainApplication.java
+++ approcketshoes/android/app/src/main/java/com/approcketshoes/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -32,11 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
         }
       };
 
